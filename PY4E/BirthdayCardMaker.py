df944c05a0fe92dbd4f6d478a0c4facca9d87706
Samuel Jansen <samuel.jansenn@gmail.com>
2019-09-06 20:59:44 -0300

    Texto Blitado

--- PY4E/BirthdayCardMaker.py
+++ PY4E/BirthdayCardMaker.py
@@ -6,7 +6,10 @@ cd PY4E
 BirthdayCardMaker.py
 """
 
-texto = """NATHALIA PACHECO BARROS
+content = """Prepara deseja um feliz aniverrsário
+aos aniversariantes
+
+NATHALIA PACHECO BARROS
 ANARETE CRISTINA TRINDADE
 ALINE CAROLINE DOS SANTOS PEREIRA
 LARISSA SILVA
@@ -25,7 +28,19 @@ ADRIANA SILVEIRA DA SILVA
 JOÃO VITOR DE MATTOS REI
 RAFAEL FABIANO CHAROPEM
 TATIANA CUNHA
-BIANCA SOUCEDO BARBOSA"""
+BIANCA SOUCEDO BARBOSA
+
+:D É os guri"""
+
+content = content.split("\n")
+print(content)
+
+
+WHITE = (255, 255, 255, 1)
+GREEN = (0, 255, 0, 1)
+BLUE = (0, 0, 128, 1)
+RED = (200, 0, 0, 1)
+TRANSPARENT = (0, 0, 0, 0)
 
 pg.init()
 
@@ -34,6 +49,17 @@ pg.font.init()
 pg.display.init()
 
 screen = pg.display.set_mode([1920,1080])
+
+
+pg.display.set_caption('Show Text')
+font = pg.font.Font('good times rg.ttf', 12)
+text = []
+textRect = []
+for i in range(len(content)) :
+    text.append(font.render(content[i], True, RED, WHITE))
+    textRect.append(text[i].get_rect())
+    textRect[i].center = (1600,100+i*25)
+
 
 #a = pg.image.load("bola01.png")
 fundo = pg.image.load("win2.png")
@@ -46,6 +72,8 @@ while not end:
         if event.type == pg.QUIT : end = True
     screen.blit(fundo,[0,0])
     screen.blit(card,[1200,60])
+    for i in range(len(text)) :
+        screen.blit(text[i], textRect[i])
     ### pg.font.Font.render(texto)
 
     pg.image.save(screen, "card1.png")
